9abe79810a28c9df5891bfc6cd87c97453968731
이상원 <87797445+knowmetoowell@users.noreply.github.com>
2024-01-22 22:41:21 +0900

    feat: 홈 화면 개발 (#4)
    bottom sheet
    주석추가
    디자인 변경 등

--- app/src/main/java/com/jinin4/journalog/db/memo/MemoDao.kt
+++ app/src/main/java/com/jinin4/journalog/db/memo/MemoDao.kt
@@ -4,7 +4,11 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
 import androidx.room.Update
+import com.jinin4.journalog.Converter
+import com.prolificinteractive.materialcalendarview.CalendarDay
 import java.sql.Timestamp
 
 // 이상원 - 24.01.19
@@ -24,6 +28,9 @@ interface MemoDao {
 
     @Query("SELECT memo_id, content, strftime('%H:%M', timestamp) as timestamp, color_id FROM memo WHERE date(timestamp) = :timestamp")
     fun getMemoByTimestamp(timestamp: String): List<MemoEntity>
+
+    @Query("SELECT DISTINCT date(timestamp) AS timestamp FROM memo")
+    fun getDistinctCalendarDays(): List<String>
 //    select date('now') as date,
 //    time('now') as time,
 //    datetime('now') as datetime;
@@ -32,7 +39,7 @@ interface MemoDao {
 //      2019-11-06  15:03:33    2019-11-06 15:03:33
 
     @Insert
-    fun insertMemo(memo: MemoEntity)
+    fun insertMemo(memo: MemoEntity): Long
 
     @Delete
     fun deleteMemo(memo: MemoEntity)
