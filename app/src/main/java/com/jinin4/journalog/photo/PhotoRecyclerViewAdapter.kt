fb129833be20ef670dc02433dc908fb53100d67b
y00n-lee <78430972+y00n-lee@users.noreply.github.com>
2024-01-22 17:56:40 +0900

    feat: 이미지 모아보기 레이아웃 및 recyclerView 데이터 바인딩 (#3)
    * 사진 모아보기 레이아웃, PhotoDao, PhotoEntity, db연결
    
    * 이미지 모아보기 레이아웃 수정 및 RecyclerView 데이터 바인딩

--- app/src/main/java/com/jinin4/journalog/photo/PhotoRecyclerViewAdapter.kt
+++ app/src/main/java/com/jinin4/journalog/photo/PhotoRecyclerViewAdapter.kt
@@ -1,16 +1,19 @@
 package com.jinin4.journalog.photo
 
-
+import android.provider.ContactsContract.Contacts.Photo
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.jinin4.journalog.R
 import com.jinin4.journalog.databinding.ItemPhotoBinding
+import com.jinin4.journalog.db.photo.PhotoEntity
 
 // 이지윤 작성 - 24.01.22
-class PhotoRecyclerViewAdapter : RecyclerView.Adapter<PhotoRecyclerViewAdapter.PhotoViewHolder>() {
+class PhotoRecyclerViewAdapter(private val photoList : ArrayList<PhotoEntity>) : RecyclerView.Adapter<PhotoRecyclerViewAdapter.PhotoViewHolder>() {
     inner class PhotoViewHolder(binding: ItemPhotoBinding) : RecyclerView.ViewHolder(binding.root) {
-
-//        val tv_title = binding.tvTitle
+        val imagePhoto: ImageView = binding.imagePhoto
         val root = binding.root
     }
 
@@ -21,10 +24,11 @@ class PhotoRecyclerViewAdapter : RecyclerView.Adapter<PhotoRecyclerViewAdapter.P
     }
 
     override fun getItemCount(): Int {
-        return 1
+        return photoList.size
     }
 
     override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
-//        holder.tv_title.text = "하이하이 테스트입니다."
+        holder.imagePhoto.setImageResource(R.drawable.test)
+
     }
 }
