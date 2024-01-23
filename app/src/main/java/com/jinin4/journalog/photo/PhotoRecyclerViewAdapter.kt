297033f919be2dd2c4a9c1342229d711ad8672af
y00n-lee <78430972+y00n-lee@users.noreply.github.com>
2024-01-23 11:13:13 +0900

    feat: 갤러리 이미지 크게보기 기능 추가 (#5)
    * 갤러리 UI 수정 및 파일 rename
    
    * recyclerView dependencies에 추가
    
    * 이미지 클릭 시 dialog 팝업 처리

--- app/src/main/java/com/jinin4/journalog/photo/PhotoRecyclerViewAdapter.kt
+++ app/src/main/java/com/jinin4/journalog/photo/PhotoRecyclerViewAdapter.kt
@@ -1,20 +1,48 @@
 package com.jinin4.journalog.photo
 
-import android.provider.ContactsContract.Contacts.Photo
-import android.util.Log
+
+import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.jinin4.journalog.R
 import com.jinin4.journalog.databinding.ItemPhotoBinding
 import com.jinin4.journalog.db.photo.PhotoEntity
 
+
 // 이지윤 작성 - 24.01.22
 class PhotoRecyclerViewAdapter(private val photoList : ArrayList<PhotoEntity>) : RecyclerView.Adapter<PhotoRecyclerViewAdapter.PhotoViewHolder>() {
+    var context: Context? = null
     inner class PhotoViewHolder(binding: ItemPhotoBinding) : RecyclerView.ViewHolder(binding.root) {
         val imagePhoto: ImageView = binding.imagePhoto
         val root = binding.root
+
+        init {
+            imagePhoto.setOnClickListener {
+                val position = bindingAdapterPosition // 현재 뷰 홀더의 위치 가져오기
+                if (position != RecyclerView.NO_POSITION) {
+
+                    // 여기서 이미지 클릭 이벤트 처리
+                    val dialogView = LayoutInflater.from(it.context)
+                        .inflate(R.layout.fragment_photo_dialog, null)
+                    val dlg = AlertDialog.Builder(it.context)
+                    val ivPic: ImageView = dialogView.findViewById<ImageView>(R.id.ivPic)
+                    Glide.with(it.context)
+                        .load(photoList[position].photo_url)
+                        .into(ivPic)
+
+                    dlg.setTitle("큰 이미지")
+//                dlg.setIcon(R.drawable.ic_launcher)
+                    dlg.setView(dialogView)
+                    dlg.setNegativeButton("닫기", null)
+                    dlg.show()
+                }
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
@@ -31,4 +59,9 @@ class PhotoRecyclerViewAdapter(private val photoList : ArrayList<PhotoEntity>) :
         holder.imagePhoto.setImageResource(R.drawable.test)
 
     }
+
+    override fun getItemId(i: Int): Long {
+        return photoList[i].photo_id?.toLong() ?: 0L
+    }
+
 }
