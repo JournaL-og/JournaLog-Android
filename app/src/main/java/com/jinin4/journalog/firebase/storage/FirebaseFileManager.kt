90624be66a22a527792073b9b335319f9a570a4e
y00n-lee <78430972+y00n-lee@users.noreply.github.com>
2024-01-24 19:19:52 +0900

    이미지 파이어베이스에서 load, 썸네일 기능 추가 (#11)

--- app/src/main/java/com/jinin4/journalog/firebase/storage/FirebaseFileManager.kt
+++ app/src/main/java/com/jinin4/journalog/firebase/storage/FirebaseFileManager.kt
@@ -2,12 +2,17 @@ package com.jinin4.journalog.firebase.storage
 
 
 import android.net.Uri
+import android.graphics.Bitmap
+import android.util.Log
 import android.widget.ImageView
 import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import com.google.firebase.storage.UploadTask
+import kotlinx.coroutines.*
+import kotlinx.coroutines.tasks.await
+import java.io.ByteArrayOutputStream
 
 
 //이상원 - 24.01.19
@@ -15,8 +20,6 @@ object FirebaseFileManager {
 
     private val firebaseStorage: FirebaseStorage = FirebaseStorage.getInstance()
     private val storageRef: StorageReference = firebaseStorage.reference
-
-
 
     fun uploadImage(imageUri: Uri, storagePath: String, onSuccess: () -> Unit, onFailure: (Exception) -> Unit) {
         val imagesRef: StorageReference = storageRef.child(storagePath)
@@ -30,6 +33,24 @@ object FirebaseFileManager {
             onFailure.invoke(exception)
         }
     }
+
+    // 이지윤: uploadImage 오버로딩 - 24.01.24
+    fun uploadImage(imageBitmap: Bitmap, storagePath: String, onSuccess: () -> Unit, onFailure: (Exception) -> Unit) {
+        val imagesRef: StorageReference = storageRef.child(storagePath)
+        val baos = ByteArrayOutputStream()
+        imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+        val uploadTask: UploadTask = imagesRef.putBytes(data)
+
+        uploadTask.addOnSuccessListener {
+            // 업로드 성공 시 처리
+            onSuccess.invoke()
+        }.addOnFailureListener { exception ->
+            // 업로드 실패 시 처리
+            onFailure.invoke(exception)
+        }
+    }
+
     //이미지뷰에 스토어의 이미지 넣는 함수
     fun loadImageIntoImageView(imageRefPath: String, imageView: ImageView) {
         val imagesRef: StorageReference = storageRef.child(imageRefPath)
@@ -43,6 +64,37 @@ object FirebaseFileManager {
             Toast.makeText(imageView.context, "이미지 불러오기 실패", Toast.LENGTH_SHORT).show()
         }
     }
+
+    // 이지윤 작성 - 24.01.24
+    fun loadImageFromFirebase(storagePath: String, onComplete: (MutableList<String>) -> Unit) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val storageRef = FirebaseStorage.getInstance().reference.child(storagePath)
+            val urls = mutableListOf<String>()
+
+            try {
+                val listResult = storageRef.listAll().await() // Wait for listAll to complete
+                Log.d("FirebaseFileManager", "Items found: ${listResult.items.size}")
+
+                val deferreds = listResult.items.map { item ->
+                    async {
+                        try {
+                            val url = item.downloadUrl.await().toString()
+                            urls.add(url)
+                            Log.d("FirebaseFileManager", "URL added: $url")
+                        } catch (e: Exception) {
+                            Log.e("FirebaseFileManager", "Failed to get download URL")
+                        }
+                    }
+                }
+                deferreds.awaitAll() // Wait for all async tasks to complete
+                urls.sort()
+                onComplete(urls)
+            } catch (e: Exception) {
+                Log.e("FirebaseFileManager", "Failed to list items: ${e.message}")
+            }
+        }
+    }
+
 
 
     // TODO:기타 파일 업로드, 다운로드 등의 함수추가피료함
--- app/src/main/java/com/jinin4/journalog/photo/PhotoRecyclerViewAdapter.kt
+++ app/src/main/java/com/jinin4/journalog/photo/PhotoRecyclerViewAdapter.kt
@@ -2,26 +2,31 @@ package com.jinin4.journalog.photo
 
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.jinin4.journalog.R
 import com.jinin4.journalog.databinding.ItemPhotoBinding
-import com.jinin4.journalog.db.photo.PhotoEntity
 
 
 // 이지윤 작성 - 24.01.22
-class PhotoRecyclerViewAdapter(private val photoList : ArrayList<PhotoEntity>) : RecyclerView.Adapter<PhotoRecyclerViewAdapter.PhotoViewHolder>() {
-    var context: Context? = null
+class PhotoRecyclerViewAdapter(private val context: Context, private val uriList : MutableList<String>)
+    : RecyclerView.Adapter<PhotoRecyclerViewAdapter.PhotoViewHolder>() {
+
     inner class PhotoViewHolder(binding: ItemPhotoBinding) : RecyclerView.ViewHolder(binding.root) {
         val imagePhoto: ImageView = binding.imagePhoto
         val root = binding.root
 
         init {
+            imagePhoto.setImageResource(R.drawable.skeleton_img)
+            // Adapter 초기화 시점에 이미지를 미리 로드합니다.
+            preloadImages()
+
             imagePhoto.setOnClickListener {
                 val position = bindingAdapterPosition // 현재 뷰 홀더의 위치 가져오기
                 if (position != RecyclerView.NO_POSITION) {
@@ -31,8 +36,10 @@ class PhotoRecyclerViewAdapter(private val photoList : ArrayList<PhotoEntity>) :
                         .inflate(R.layout.fragment_photo_dialog, null)
                     val dlg = AlertDialog.Builder(it.context)
                     val ivPic: ImageView = dialogView.findViewById<ImageView>(R.id.ivPic)
+                    Log.d("FirebaseImageLoad","uriList[position]: ${uriList[position]}, ${position}")
                     Glide.with(it.context)
-                        .load(photoList[position].photo_url)
+                        .load(uriList[position])
+                        .placeholder(R.drawable.skeleton_img)
                         .into(ivPic)
 
                     dlg.setTitle("큰 이미지")
@@ -52,16 +59,39 @@ class PhotoRecyclerViewAdapter(private val photoList : ArrayList<PhotoEntity>) :
     }
 
     override fun getItemCount(): Int {
-        return photoList.size
+        return uriList.size
     }
 
     override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
-        holder.imagePhoto.setImageResource(R.drawable.test)
+        val imageUrl = uriList[position]
 
+        holder.apply {
+            Glide.with(holder.itemView.context)
+                .load(imageUrl)
+                .override(150, 150)
+                .placeholder(R.drawable.skeleton_img)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .skipMemoryCache(false)
+                .into(holder.imagePhoto)
+        }
     }
 
-    override fun getItemId(i: Int): Long {
-        return photoList[i].photo_id?.toLong() ?: 0L
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    private fun preloadImages() {
+        uriList.forEach { imageUrl ->
+            Glide.with(context)
+                .load(imageUrl)
+                .override(150, 150)
+                .placeholder(R.drawable.skeleton_img)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .skipMemoryCache(false)
+                .preload()
+        }
     }
 
 }
+
+
