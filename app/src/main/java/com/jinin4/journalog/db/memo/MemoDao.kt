dc5bddb13bdf205b71a0765d4aad291da20e9868
knowmetoowell <87797445+knowmetoowell@users.noreply.github.com>
2024-01-28 16:44:08 +0900

    feat 홈화면
    정렬 기능

--- app/src/main/java/com/jinin4/journalog/db/memo/MemoDao.kt
+++ app/src/main/java/com/jinin4/journalog/db/memo/MemoDao.kt
@@ -14,22 +14,23 @@ import java.sql.Timestamp
 // 이상원 - 24.01.19
 @Dao
 interface MemoDao {
-    @Query("select memo_id,content,timestamp,color_id from memo")
+    @Query("select memo_id,content,timestamp,color_id from memo order by timestamp")
+    fun getAllMemoAsc(): List<MemoEntity>
+    @Query("select memo_id,content,timestamp,color_id from memo order by timestamp desc")
     fun getAllMemo(): List<MemoEntity>
-
-
     @Query("select memo_id,content,timestamp,color_id from memo where memo_id = :id")
     fun getMemoById(id: Int): MemoEntity
     // 최성혁 수정 - 24.01.22
-    @Query("select memo_id,content,timestamp,color_id from memo where color_id = :id")
+    @Query("select memo_id,content,timestamp,color_id from memo where color_id = :id order by timestamp")
+    fun getMemosByColorIdAsc(id: Int): List<MemoEntity>
+    @Query("select memo_id,content,timestamp,color_id from memo where color_id = :id order by timestamp desc")
     fun getMemosByColorId(id: Int): List<MemoEntity>
 
-//    @Query("select memo_id,content,date(timestamp),color_id from memo where timestamp = :timestamp")
-//    fun getMemoByTimestamp(timestamp: String): List<MemoEntity>
 
     @Query("SELECT memo_id, content, strftime('%H:%M', timestamp) as timestamp, color_id FROM memo WHERE date(timestamp) = :timestamp order by timestamp desc")
     fun getMemoByTimestamp(timestamp: String): List<MemoEntity>
-
+    @Query("SELECT memo_id, content, strftime('%H:%M', timestamp) as timestamp, color_id FROM memo WHERE date(timestamp) = :timestamp order by timestamp")
+    fun getMemoByTimestampAsc(timestamp: String): List<MemoEntity>
     @Query("SELECT DISTINCT date(timestamp) AS timestamp FROM memo")
     fun getDistinctCalendarDays(): List<String>
 //    select date('now') as date,
